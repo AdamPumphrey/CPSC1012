650906e0cbb1c974ba81b7de9211bf6146acd754
AdamPumphrey <a.pumphrey15@gmail.com>
2020-09-15 16:02:25 -0600

    Added AcreConversion

---
+++ AcreConversion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Purpose:        Convert given square foot value to acres
+Input:          sqft
+Output:         acres
+Written By:     Adam Pumphrey
+Last Modified:  Sept. 15, 2020
+*/
+
+namespace AcreConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 acre = 43560 sq ft
+            const int ConversionRatio = 43560;
+
+            double sqft,
+                   acres;
+
+            Console.Write("Enter Square Footage: ");
+            sqft = double.Parse(Console.ReadLine());
+
+            acres = sqft / ConversionRatio;
+
+            Console.WriteLine("{0} square feet = {1:0.00} acres", sqft, acres);
+
+            Console.ReadLine();
+        }
+    }
+}
