ec7e1eb630ecd6b1662f9644374921eb558560ab
AdamPumphrey <a.pumphrey15@gmail.com>
2020-09-15 15:33:18 -0600

    added practice problem - constants and str formatting

---
+++ WestCoastSales/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Purpose:        Calculate West Coast sales based on total sales
+Input:          N/A
+Output:         sales
+Written By:     Adam Pumphrey
+Last Modified:  Sept. 15, 2020
+*/ 
+
+namespace WestCoastSales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double Percentage = 0.43,
+                TotalSales = 5300000;
+
+            double sales;
+
+            sales = TotalSales * Percentage;
+
+            // :c displays value as currency
+            Console.WriteLine("West Coast sales is {0:c}", sales);
+
+            // :0.00 displays value with specified number of decimal places (rounded)
+            Console.WriteLine("West Coast sales is {0:0.00}", sales);
+
+            Console.ReadLine();
+        }
+    }
+}
