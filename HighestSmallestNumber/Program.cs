d2f9770c05ff65044d2188a895e4866ac2b4c1a2
AdamPumphrey <a.pumphrey15@gmail.com>
2020-09-22 15:48:54 -0600

    Added HighestSmallestNumber

---
+++ HighestSmallestNumber/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Purpose: Determine the highest and smallest number from three inputted values
+Input: number1, number2, number3
+Output: highest, smallest
+Written By: Adam Pumphrey
+Last Modified: Sept. 22, 2020
+*/
+
+namespace HighestSmallestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1,
+                number2,
+                number3,
+                highest,
+                smallest;
+
+            Console.Write("Enter number1: ");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number2: ");
+            number2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number3: ");
+            number3 = int.Parse(Console.ReadLine());
+
+            highest = number1;
+            smallest = number1;
+
+            if (number2 > highest)
+            {
+                highest = number2;
+            }
+            else if (number2 < smallest)
+            {
+                smallest = number2;
+            }
+
+            if (number3 > highest)
+            {
+                highest = number3;
+            }
+            else if (number3 < smallest)
+            {
+                smallest = number3;
+            }
+
+            Console.WriteLine("The highest is {0} and the smallest is {1}", highest, smallest);
+
+            Console.ReadLine();
+        }
+    }
+}
