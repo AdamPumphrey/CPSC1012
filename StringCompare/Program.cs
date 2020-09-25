aae4f57f58fc629965f9faa8d7af351b01e51521
AdamPumphrey <a.pumphrey15@gmail.com>
2020-09-25 14:56:45 -0600

    Added SwitchDemo

---
+++ StringCompare/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Purpose: String.Compare() demo
+Input: string1, string2
+Output: compare
+Written By: Adam Pumphrey
+Last Modified: Sept. 24, 2020
+*/
+
+namespace StringCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1,
+                   string2,
+                   first,
+                   second;
+
+            Console.Write("Enter string1: ");
+            string1 = Console.ReadLine();
+            Console.Write("Enter string2: ");
+            string2 = Console.ReadLine();
+
+            if (string.Compare(string1, string2) < 0)
+            {
+                first = string1;
+                second = string2;
+            }
+            else if (string.Compare(string1, string2) > 0)
+            {
+                first = string2;
+                second = string1;
+            }
+            else
+            {
+                first = "Same string";
+                second = first;
+            }
+
+            Console.WriteLine("First = {0}\t\tSecond = {1}", first, second);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SwitchDemo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Purpose:        Demonstrate use of switch() with int and char data types
+Input:          intOption, charOption
+Output:         message
+Written By:     Adam Pumphrey
+Last Modified:  Sept. 25, 2020
+*/
+
+namespace SwitchDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Switch Demos";
+
+            int intOption;
+
+            char charOption;
+
+            string message;
+
+            Console.WriteLine("Select from one of the following options:");
+            Console.WriteLine("\t1. First int item");
+            Console.WriteLine("\t2. Second int item");
+            Console.WriteLine("\t3. Third int item");
+            Console.Write("Option: ");
+
+            intOption = int.Parse(Console.ReadLine());
+
+            switch (intOption)
+            {
+                case 1:
+                    message = "First int item";
+                    break;
+
+                case 2:
+                    message = "Second int item";
+                    break;
+
+                case 3:
+                    message = "Third int item";
+                    break;
+
+                default:
+                    message = "Invalid option";
+                    break;
+            }
+
+            Console.WriteLine("\nInt menu output:");
+            Console.WriteLine("{0}\n", message);
+
+            Console.WriteLine("Select from one of the following options:");
+            Console.WriteLine("\tA. First char item");
+            Console.WriteLine("\tB. Second char item");
+            Console.WriteLine("\tC. Third char item");
+            Console.Write("Option: ");
+
+            charOption = char.Parse(Console.ReadLine().ToUpper());
+
+            switch (charOption)
+            {
+                case 'A':
+                    message = "First char item";
+                    break;
+
+                case 'B':
+                    message = "Second char item";
+                    break;
+
+                case 'C':
+                    message = "Third char item";
+                    break;
+
+                default:
+                    message = "Invalid option";
+                    break;
+            }
+
+            Console.WriteLine("\nChar menu output:");
+            Console.WriteLine("{0}\n", message);
+
+            Console.ReadLine();
+        }
+    }
+}
