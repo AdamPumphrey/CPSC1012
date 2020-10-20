49348a3de7a1905e6bd02ca014f3cbd5363f568c
AdamPumphrey <a.pumphrey15@gmail.com>
2020-10-20 15:19:35 -0600

    Added methoddemo

---
+++ MethodDemo_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+Purpose:		Demonstrate some simple methods:
+                    GetSafeInt(string prompt)
+                    GetSafeInt(string prompt, int minValue, int maxValue)
+                    GetSafeChar(string prompt)
+                    DisplayMenu()
+                    ProcessMenu(int menuOption, int first, int second)
+                    DisplayResults(int menuOption, int result)
+                    ConsoleSetup() [optional]
+Input:			two validiated numbers, menu option (exit on 0)
+Output:			calcualtion based on the menu option:
+                1. first^second
+                2. second^first
+                3. first^2 + second^2
+Written By: 	 
+Last Modified:	Oct 20,2020
+*/
+
+namespace MethodDemo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare constants
+
+            //main program loop
+
+
+            //program ends
+            Console.WriteLine("\nGoodbye ...");
+            Console.ReadLine();
+        }//eom
+    }//eoc
+}//eon
