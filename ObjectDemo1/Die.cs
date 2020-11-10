51660a7f077fe282f78ccb529a5219232468fac4
AdamPumphrey <a.pumphrey15@gmail.com>
2020-11-10 16:57:35 -0700

    added DieClassDemo

---
+++ ObjectDemo1/Die.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieClassDemo
+{
+    public class Die
+    {
+        // private member fields
+        private int _sides;
+        private int _faceValue;
+
+        // public Accessors and Mutators
+        public int Sides
+        {
+            get { return _sides; } // public Accessor
+            // public Mutator
+            set
+            {
+                if (value >= 4)
+                {
+                    _sides = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid sides for a Die");
+                }
+            }
+        }
+
+        public int FaceValue
+        {
+            get { return _faceValue; }
+            set
+            {
+                if (value > 0 && value <= _sides)
+                {
+                    _faceValue = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid face value");
+                }
+            }
+        }
+
+        // Constructor(s)
+        // Empty constructor
+        public Die()
+        {
+            Sides = 6;
+            FaceValue = 1;
+        }
+
+        // Greedy constructor
+        public Die(int sides)
+        {
+            Sides = sides;
+            FaceValue = 1;
+        }
+
+        // Class method(s)
+        public void Roll()
+        {
+            Random rnd = new Random();
+            FaceValue = rnd.Next(1, Sides + 1);
+        }
+
+        public override string ToString()
+        {
+            return "Sides = " + Sides + ", FaceValue = " + FaceValue;
+        }
+    }
+}
---
+++ ObjectDemo1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+Purpose:		Demonstrate the creation and use of a simple class	 
+Input:			Die class
+				+-----------------------+
+				|          Die          |
+				+-----------------------+
+				| - sides: Integer      |
+				| - faceValue: Integer  |
+				| + Sides: Integer      |
+				| + FaceValue: Integer  |
+				+-----------------------+
+				| + Die()               |
+				| + Die(Sides: Integer) |
+				| + Roll()              |
+				+-----------------------+
+				
+Output:			values of the instances of the Die class 
+Written By: 	 
+Last Modified:	 
+*/
+
+namespace DieClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Setup();
+
+            //Create and display a default instance of the class
+            Die sixSidedDie = new Die();
+            Console.WriteLine("Sides = {0}, FaceValue = {1}", sixSidedDie.Sides, sixSidedDie.FaceValue);
+
+            //Display the instance of the class using a class method
+
+
+            //Create and display a non-default instance of the class
+            Die someDie = new Die(20);
+            Console.WriteLine("Sides = {0}, FaceValue = {1}", someDie.Sides, someDie.FaceValue);
+
+            //Change the values(s) of the instances of the class
+            sixSidedDie.Roll();
+            someDie.Roll();
+            Console.WriteLine("Sides = {0}, FaceValue = {1}", sixSidedDie.Sides, sixSidedDie.FaceValue);
+            Console.WriteLine("Sides = {0}, FaceValue = {1}", someDie.Sides, someDie.FaceValue);
+
+            Console.ReadLine();
+        }//eom
+
+        #region Provided methods
+        static void Setup()
+        {
+            Console.Title = "Die Class Demo";
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+        }//end of Setup
+        #endregion
+    }//eoc
+}//eon
