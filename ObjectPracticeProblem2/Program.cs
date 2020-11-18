c8f3f58a01c5d0688522b779d877491a1e39125e
AdamPumphrey <a.pumphrey15@gmail.com>
2020-11-18 15:12:50 -0700

    Added Object Practice Problem 2

---
+++ ObjectPracticeProblem2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Purpose:        Create a program that will allow an instructor to enter the quiz marks for a class
+Input:          total, weight, name, mark
+Output:         total, weight, name, mark, mark percentage, average mark, average percantage
+Written By:     Adam Pumphrey
+Last Modified:  Nov. 18, 2020
+*/
+
+namespace ObjectPracticeProblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total,
+                weight,
+                mark;
+
+            double sum = 0,
+                   avgPercent,
+                   avg;
+
+            string name;
+
+            bool isValid = false;
+
+            List<Quiz> quizzes = new List<Quiz>();
+
+            Console.WriteLine("Worksheet 11");
+            total = GetSafeInt("Enter Quiz Total: ");
+            weight = GetSafeInt("Enter Quiz Weight: ");
+            Console.WriteLine();
+
+            do
+            {
+                name = GetSafeString("Enter student's name (zzz to quit): ");
+                if (name.ToLower().Equals("zzz"))
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    mark = GetSafeInt("Enter student's mark: ");
+                    sum += mark;
+                    Quiz quiz = new Quiz(mark, total, weight, name);
+                    quizzes.Add(quiz);
+                }
+            } while (!isValid);
+
+            Console.WriteLine("\nClass Marks");
+            Console.WriteLine("Quiz Total: {0}\tQuiz Weight: {1}", total, weight);
+            Console.WriteLine("Name\tMark\t%");
+            foreach (Quiz item in quizzes)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", item.StudentName, item.Mark, item.GetPercentage());
+            }
+
+            avg = sum / quizzes.Count();
+
+            avgPercent = (avg / total) * 100;
+
+            Console.WriteLine("\nClass Average: {0:0.00} = {1:0.00}%", avg, avgPercent);
+
+            Console.ReadLine();
+        }
+
+        static int GetSafeInt(string prompt)
+        {
+            bool isValid = false;
+            int number = -99;
+            do
+            {
+                try
+                {
+                    // validate int input
+                    Console.Write(prompt);
+                    number = int.Parse(Console.ReadLine());
+                    isValid = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nInvalid input .. please try again.");
+                }
+            } while (!isValid);
+
+            return number;
+        }
+
+        static string GetSafeString(string prompt)
+        {
+            bool isValid = false;
+            string name;
+            do
+            {
+                Console.Write(prompt);
+                name = Console.ReadLine();
+                if (name.Length > 0)
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid input .. please try again.");
+                }
+            } while (!isValid);
+            return name;
+        }
+    }
+}
---
+++ ObjectPracticeProblem2/Quiz.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Purpose:        Create Quiz class
+Input:          None in this file, but would be at least mark, total, weight, studentName
+Output:         None in this file, but would be mark percentage
+Written By:     Adam Pumphrey
+Last Modified:  Nov. 13, 2020
+*/
+
+namespace ObjectPracticeProblem2
+{
+    public class Quiz
+    {
+        private string _studentName;
+        private int _total,
+                    _mark,
+                    _weight;
+
+        public string StudentName
+        {
+            get { return _studentName; }
+            set
+            {
+                if (value.Length >= 1)
+                {
+                    _studentName = value;
+                }
+                else
+                {
+                    throw new Exception("Name must be at least 1 character");
+                }
+            }
+        }
+
+        public int Total
+        {
+            get { return _total; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    _total = value;
+                }
+                else
+                {
+                    throw new Exception("Total must be at least 0 and at most 100");
+                }
+            }
+        }
+
+        public int Mark
+        {
+            get { return _mark; }
+            set
+            {
+                if (value >= 0 && value <= Total)
+                {
+                    _mark = value;
+                }
+                else
+                {
+                    throw new Exception("Mark must be at least 0 and at most the same as the total");
+                }
+            }
+        }
+
+        public int Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _weight = value;
+                }
+                else
+                {
+                    throw new Exception("Weight must be at least 0");
+                }
+            }
+        }
+
+        public Quiz(int mark, int total, int weight, string studentName)
+        {
+            // total set first so that mark has a maximum value
+            Total = total;
+            Mark = mark;
+            Weight = weight;
+            StudentName = studentName;
+        }
+
+        public double GetPercentage()
+        {
+            return ((double)Mark / Total) * 100;
+        }
+    }
+}
